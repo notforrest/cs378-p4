6466c78ccc1094ef064f1e0c90a04fdde44bb761
Forrest Sun <Ddeosun@gmail.com>
2024-03-11 10:28:33 -0600

    Refactor CSS and add accessibility feature

--- src/App.js
+++ src/App.js
@@ -5,27 +5,32 @@ import React, { useState, useEffect, Fragment } from "react";
 function App() {
     // Initialize state variables for activity and accessibility
     const [activityList, setActivityList] = useState([]);
+    const [category, setCategory] = useState("");
+    const [maxAccessLevel, setMaxAccessLevel] = useState(1);
+    const [specificSelected, setSpecificSelected] = useState(false);
 
     // Write an API call to the Bored API
-    async function fetchActivityList(type = "", participants = 0) {
+    async function fetchActivityList(type) {
         try {
             let activities = [];
             while (activities.length < 10) {
                 const response = await fetch(
-                    `https://www.boredapi.com/api/activity${
-                        type ? `?type=${type}` : ""
+                    `https://www.boredapi.com/api/activity?minaccessibility=0&maxaccessibility=${maxAccessLevel}${
+                        type ? `&type=${type}` : ""
                     }`
                 );
+
                 const data = await response.json();
 
                 if (!activities.includes(data.activity)) {
-                    activities.push(data.activity);
+                    activities.push([data.activity, data.accessibility]);
                 }
             }
             console.log(activities);
 
             // Set the state variables to the data from the API
             setActivityList(activities);
+            console.log(activityList);
         } catch (error) {
             console.log(error);
         }
@@ -36,18 +41,44 @@ function App() {
     }, []);
 
     // Generate a random activity after pressing the button
-    const generateActivityList = async (type) => {
+    const generateActivityList = async (type = "") => {
         await fetchActivityList(type);
+
+        switch (type) {
+            case "social":
+                setCategory("social");
+                break;
+            case "education":
+                setCategory("educational");
+                break;
+            case "relaxation":
+                setCategory("relaxing");
+                break;
+            default:
+                setCategory("");
+                break;
+        }
     };
 
     return (
         <div className="App">
             <div className="Page">
                 <h1>Let's get unbored!</h1>
-                <h2>Here are 10 random activities you could do!</h2>
+                {specificSelected ? (
+                    <h2>
+                        Here are 10 activities at an accessibility level of{" "}
+                        {maxAccessLevel} or less!
+                    </h2>
+                ) : (
+                    <h2>Here are 10 {category} activities you could do!</h2>
+                )}
                 <div>
                     {activityList.map((activity, index) => {
-                        return <p key={index}>{activity}</p>;
+                        return (
+                            <p key={index}>
+                                {activity[0] + ` (${activity[1]})`}
+                            </p>
+                        );
                     })}
                 </div>
                 <h3>
@@ -78,9 +109,35 @@ function App() {
                     </button>
                 </div>
                 <h3>
-                    Search for an activity with a specific # of people or more
+                    Search for an activity with a specific accessibility level
+                    or less (0.00 - 1.00)
                 </h3>
-                <input style={{ marginBottom: "3em" }} />
+                <div
+                    style={{
+                        alignItems: "center",
+                        display: "flex",
+                        gap: "1em",
+                        justifyContent: "center",
+                        marginBottom: "2em",
+                    }}
+                >
+                    <input
+                        type="number"
+                        step="0.05"
+                        min="0"
+                        max="1"
+                        onChange={(e) => setMaxAccessLevel(e.target.value)}
+                    />
+                    <button
+                        className="pplBtn"
+                        onClick={() => {
+                            generateActivityList();
+                            setSpecificSelected(true);
+                        }}
+                    >
+                        Search
+                    </button>
+                </div>
             </div>
         </div>
     );
